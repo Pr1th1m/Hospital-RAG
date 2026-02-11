8f25c977696bb69c7ab9dbe1154c3abbf80fc0a4
Pr1th1m <147029188+Pr1th1m@users.noreply.github.com>
2026-02-11 16:53:37 +0530

    department_model updated

--- backend/pydantic_models/department_model.py
+++ backend/pydantic_models/department_model.py
@@ -5,7 +5,7 @@ from uuid import UUID, uuid4
 
 class Department(BaseModel):
     # department_id: UUID = Field(default_factory=uuid4)
-    # hospital_id: UUID
+    hospital_id: UUID
     department_name: str    # cardiology,neurology,orthopedics,etc
     services: Optional[List[str]] = []
     icu_support: bool
