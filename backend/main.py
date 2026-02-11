f5abaf380b7bcc0c37c0d446255a5cc424908087
Pr1th1m <147029188+Pr1th1m@users.noreply.github.com>
2026-02-11 16:50:21 +0530

    main.py modified

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,7 @@ from database_models.doctor_database_model import Doctor as DoctorDB
 from database_models.department_database_model import Department as DepartmentDB
 from database_models.base import Base
 from vector_database import transform_text
-from system_prompt import system_prompt_hospital,system_prompt_department,system_prompt_doctor
-
+from system_prompt import system_prompt_hospital,system_prompt_department,system_prompt_doctor,system_prompt_hospital_list,system_prompt_department_list,system_prompt_doctor_list
 
 Base.metadata.create_all(bind=engine)
 
@@ -33,7 +32,7 @@ def add_hospital(hospital:Hospital, db:Session = Depends(get_db)):
     if hospital:
         db.add(HospitalDB(**hospital.model_dump()))
         db.commit()
-        transform_text(hospital,system_prompt_hospital)
+        # transform_text(hospital,system_prompt_hospital)
         return {"message": "Hospital created successfully"}
     return {"message": "Hospital details not found"}
 
@@ -49,7 +48,7 @@ def add_department(department:Department, db:Session = Depends(get_db)):
         department_merged = hospital.__dict__ | department.__dict__
         db.add(DepartmentDB(**department.model_dump()))
         db.commit()
-        transform_text(department_merged,system_prompt_department)
+        # transform_text(department_merged,system_prompt_department)
         return {"message": f"Department \n{department}\n created successfully"}   
     return {"message": "Department details not found"}    
 
@@ -66,6 +65,43 @@ def add_doctor(doctor:Doctor, db:Session = Depends(get_db)):
         doctor_merged = hospital.__dict__ | department.__dict__ | doctor.__dict__
         db.add(DoctorDB(**doctor.model_dump()))
         db.commit()
-        transform_text(doctor_merged,system_prompt_doctor)
+        # transform_text(doctor_merged,system_prompt_doctor)
         return {"message": f"Doctor \n{doctor}\n created successfully"}
     return {"message": "Doctor details not found"}
+
+@app.get('/get_hospitals')
+def get_hospitals(db:Session = Depends(get_db)):
+    hosp = []
+    hospitals = db.query(HospitalDB).all()
+    for hospital in hospitals:
+        hosp.append(str(hospital.__dict__))
+    transform_text(str(hosp),system_prompt_hospital_list)
+    return hospitals
+
+@app.get('/get_departments')
+def get_departments(db:Session = Depends(get_db)):
+    dept = []
+    departments = db.query(DepartmentDB).all()
+    for department in departments:
+        hospital = db.query(HospitalDB).filter(
+            HospitalDB.hospital_id == department.hospital_id
+        ).first()
+        dept.append(str(department.__dict__ | hospital.__dict__))
+    transform_text(str(dept),system_prompt_department_list)
+    return departments
+
+@app.get('/get_doctors')
+def get_doctors(db:Session = Depends(get_db)):
+    docs = []
+    doctors = db.query(DoctorDB).all()
+    for doctor in doctors:
+        hospital = db.query(HospitalDB).filter(
+            HospitalDB.hospital_id == doctor.hospital_id
+        ).first()
+        department = db.query(DepartmentDB).filter(
+            DepartmentDB.department_id == doctor.department_id
+        ).first()
+        docs.append(str(doctor.__dict__ | hospital.__dict__ | department.__dict__))
+    for doc in docs:
+        transform_text(str(doc),system_prompt_doctor_list)
+    return {"Message": "Doctors fetched successfully"}
